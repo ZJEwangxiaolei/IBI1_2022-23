318cdf78c951567236775554210fbc928a11be14
王晓磊 <wangxiaolei@wangxiaoleideMacBook-Pro.local>
2023-03-15 18:51:29 +0800

    variables.py

---
+++ Practical5/ variables.py
@@ -0,0 +1,12 @@
+a=-3.19
+b=-118.24
+c=116.39
+d=a-b
+e=a-c
+def compare(a,b):
+if a > b:
+print a, ">", b
+elif a == b:
+print a, "=", b
+elif a < b:
+print a, "<", b
---
+++ Practical5/variables.py
@@ -0,0 +1,19 @@
+a=-3.19
+b=-118.24
+c=116.39
+d=a-b
+e=a-c
+
+if d > e:
+    print("Rob travelled further to Los Angeles than to Haining.")
+elif d < e:
+    print("Rob travelled further to Haining than to Los Angeles.")
+else:
+    print("Rob travelled the same distance to Los Angeles and Haining.")
+
+X=True
+Y=False
+W=X and Y
+Z=X or Y
+print("W=",W)
+print("Z=",Z)
