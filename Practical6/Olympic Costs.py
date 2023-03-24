e1ef53f3a2f7f7d5bc3866cf5b29b5ed196ba034
王晓磊 <wangxiaolei@wangxiaoleideMacBook-Pro.local>
2023-03-24 16:31:33 +0800

    ZJEwangxiaolei

--- Practical6/Olympic Costs.py
+++ Practical6/Olympic Costs.py
@@ -1,14 +1,15 @@
 # pseudocode 
-# create a lidt to store data
+# create a list to store data
 costs=[1,8,15,7,5,14,43,40]
+# cities=['Los Angeles 1984', 'Seoul 1988', 'Barcelona 1992', 'Atlanta 1996', 'Sydney 2000', 'Athens 2003', 'Beijing 2008', 'London 2012']
+dictionary={'Los Angeles 1984':1, 'Seoul 1988':8, 'Barcelona 1992':15, 'Atlanta 1996':7, 'Sydney 2000':5, 'Athens 2003':14, 'Beijing 2008':43, 'London 2012':40}
+sorted_dict = dict(sorted(dictionary.items(), key=lambda x: int(x[1])))
 import numpy as np
 import matplotlib.pyplot as plt
-N=8
-ind =np.arange(N)
-width=0.1
-p1=plt.bar(ind,costs,width)
+# creat the x,y ticks of the bar plot
 plt.ylabel('costs')
 plt.title('Olympic Costs')
-plt.xticks(ind,('Los Angeles 1984', 'Seoul 1988', 'Barcelona 1992', 'Atlanta 1996', 'Sydney 2000', 'Athens 2003', 'Beijing 2008', 'London 2012'))
+plt.xlabel('cities')
+plt.bar(sorted_dict.keys(),sorted_dict.values(),0.5)
 plt.yticks(np.arange(0,45,5))
 plt.show()
