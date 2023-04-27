e930e8840f71cf81bd7aa2d32c859843101a7ef6
王晓磊 <wangxiaolei@wangxiaoleideMacBook-Pro.local>
2023-04-27 11:45:35 +0800

    ZJEwangxiaolei

--- Practical11/Alignment.py
+++ Practical11/Alignment.py
@@ -1,29 +1,19 @@
-import blosum as bl
+from Bio import Align
+from Bio.Seq import Seq
+aligner = Align.PairwiseAligner()
 def read_seq_from_file(filename):
     with open(filename, "r") as file:
         lines = file.readlines()
         sequence = lines[1].replace("\n", "")
     return sequence
-
 seq_cat=read_seq_from_file("/Users/wangxiaolei/IBI1_2022-23/Practical11/ACE2_cat.fa")
 seq_human=read_seq_from_file("/Users/wangxiaolei/IBI1_2022-23/Practical11/ACE2_human.fa")
 seq_mouse=read_seq_from_file('/Users/wangxiaolei/IBI1_2022-23/Practical11/ACE2_mouse.fa')
-len1 = len(seq_cat)
-len2 = len(seq_human)
-len3 = len(seq_mouse)
-edit_distance1 = 0
-for i in range(len1):
-    if seq_cat[i]==seq_human[i]:
-     edit_distance1 += 1
-print (edit_distance1)
-edit_distance2 = 0
-for i in range(len3):
-    if seq_mouse[i] ==seq_human[i]:
-     edit_distance2 += 1
-print (edit_distance2)
-if edit_distance1>edit_distance2:
+alignments1 = aligner.score(seq_cat, seq_human)
+print(alignments1)
+alignments2 = aligner.score(seq_mouse, seq_human)
+print(alignments2)
+if alignments1>alignments2:
    print("human's ACE2 gene sequence more like the cat")
-if edit_distance1<edit_distance2:
+if alignments1<alignments2:
    print("human's ACE2 gene sequence more like the mouse")
-
-
